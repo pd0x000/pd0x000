0c6d45beefee03c1c7a4c3e6a663393869eaa443
PxDxUST <81381886+pd0x000@users.noreply.github.com>
2021-11-16 15:43:32 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+ function addClass() {
+  const menuBody = document.getElementById("menu__body");
+  const menuIcon = document.getElementById("menu_icon");
+   document.body.classList.toggle('_lock');
+   menuBody.classList.toggle("_active");
+   menuIcon.classList.toggle("_active");
+        }
