2463bfb13b1e9e4c3440c0eda976aee3f2954fd5
PxDxUST <81381886+pd0x000@users.noreply.github.com>
2021-11-17 03:22:22 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
-
- function addClass() {
   const menuBody = document.getElementById("menu__body");
   const menuIcon = document.getElementById("menu_icon");
+ function addClass() {
+
    document.body.classList.toggle('_lock');
    menuBody.classList.toggle("_active");
    menuIcon.classList.toggle("_active");
+        }
+
+ function remClass() {
+
+   document.body.classList.remove('_lock');
+   menuBody.classList.remove("_active");
+   menuIcon.classList.remove("_active");
         }
